c4dafb7a7c8d3d0a14bd8100f04e657569a21444
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2024-08-09 17:18:01 +0100

    chore: testing workflow

--- .github/actions/create-env-action/index.js
+++ .github/actions/create-env-action/index.js
@@ -1,45 +1,50 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const { writeFile } = require('node:fs/promises');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { writeFile } = require("node:fs/promises");
 
-try {
-    const envFileName = core.getInput('env_file_name') || "app.env"
+async function run() {
+  try {
+    const envFileName = core.getInput("env_file_name") || "app.env";
 
     const secrets = [
-        "APP_NAME",
-        "APP_URL",
-        "SERVER_PORT",
-        "DB_NAME",
-        "USERNAME",
-        "REDIS_PORT",
-        "REDIS_HOST",
-        "REDIS_DB",
-        "GOOGLE_CLIENT_ID",
-        "GOOGLE_CLIENT_SECRET",
-        "FACEBOOK_CLIENT_ID",
-        "FACEBOOK_CLIENT_SECRET",
-        "SESSION_SECRET",
-        "MAIL_SERVER",
-        "MAIL_USERNAME",
-        "MAIL_PASSWORD",
-        "MAIL_PORT",
-        "MIGRATE"
-      ];
-      
+      "APP_NAME",
+      "APP_URL",
+      "SERVER_PORT",
+      "DB_NAME",
+      "USERNAME",
+      "REDIS_PORT",
+      "REDIS_HOST",
+      "REDIS_DB",
+      "GOOGLE_CLIENT_ID",
+      "GOOGLE_CLIENT_SECRET",
+      "FACEBOOK_CLIENT_ID",
+      "FACEBOOK_CLIENT_SECRET",
+      "SESSION_SECRET",
+      "MAIL_SERVER",
+      "MAIL_USERNAME",
+      "MAIL_PASSWORD",
+      "MAIL_PORT",
+      "MIGRATE",
+    ];
 
-    let envString = ""
+    let envString = "";
 
-    for (let secret of secrets){
-        const value = core.getInput(secret)
-        envString += `${secret}=${value}\n`
+    for (let secret of secrets) {
+      const value = core.getInput(secret);
+      envString += `${secret}=${value}\n`;
     }
 
-    const envFile = await writeFile(envFileName, envString, { encoding: 'utf-8' })
+    const envFile = await writeFile(envFileName, envString, {
+      encoding: "utf-8",
+    });
 
     core.setOutput("env_file", envFile);
-    
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2);
     console.log(`The event payload: ${payload}`);
-} catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
+  }
 }
+
+run();
